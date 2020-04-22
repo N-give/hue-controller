cce7b434cfe27bbaeaf0f33824a95cf860d1a11f
Nathan Givens <nate.givens03@gmail.com>
2020-04-22 07:48:08 -0400

    hello world app
    issue with overmind prevents compilation

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { createOvermind } from 'overmind';
+import { Provider } from 'overmind-react';
+import { config } from './overmind';
+
+import App from './App';
+
+const overmind = createOvermind(config);
+
+render((
+  <Provider value={overmind}>
+    <App />
+  </Provider>
+), document.getElementById('root'));
---
+++ src/overmind/actions.ts
@@ -0,0 +1,3 @@
+export const firstAction = ({}) => {
+  // TODO
+};
