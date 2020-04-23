3db73621782aec390fd609af409ad93e212b9675
Nathan Givens <nate.givens03@gmail.com>
2020-04-23 01:58:02 -0400

    fixed compilation error

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,21 @@
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   entry: path.resolve(__dirname, "src", "./index.tsx"),
-  devtool: "inline-source-map",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  resolve: {
-    modules: [__dirname, "src", "node_modules"],
-    extensions: ["*", ".ts", ".tsx", ".js"],
-  },
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     template: "./dist/index.html",
-  //   }),
-  // ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
+        use: "ts-loader",
         exclude: /node_modules/,
-        loader: "ts-loader",
-      },
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader",
       },
     ],
   },
-  externals: {
-    react: "React",
-    "react-dom": "ReactDom",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
